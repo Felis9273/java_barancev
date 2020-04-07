540f072b308a5fee972975c0704e976e0a7e8bc4
Felis9273 <45122166+Felis9273@users.noreply.github.com>
2020-04-07 22:37:04 +0300

    MyFirst program changed

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,9 +1,12 @@
 package ru.stqa.pft.sandbox;
 
+import static ru.stqa.pft.sandbox.Point.distance;
+
 public class MyFirstProgram {
 
-public static void main(String[] args){
-
-	System.out.println("Hello, World!");
-      }
+  public static void main(String[] args) {
+    Point p1 = new Point(2,1);
+    Point p2 = new Point(6,4);
+    System.out.println("Расстояние между точками равно "+distance(p1,p2));
+  }
 }
