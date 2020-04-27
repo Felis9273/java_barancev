fe6a662155bf83b6f40ae4548b33c55f427d0536
Felis9273 <45122166+Felis9273@users.noreply.github.com>
2020-04-27 22:06:45 +0300

    Created test for Contact creation
    New test for Contact creation was added.
    Parametres of Contact were gathered into ContactData object

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,67 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String name;
+  private final String lastname;
+  private final String nickname;
+  private final String company;
+  private final String address;
+  private final String fonenumber;
+  private final String birthday;
+  private final String month;
+  private final String year;
+  private final String addInfo;
+
+  public ContactData(String name, String lastname, String nickname, String company, String address, String fonenumber, String birthday, String month, String year, String addInfo) {
+    this.name = name;
+    this.lastname = lastname;
+    this.nickname = nickname;
+    this.company = company;
+    this.address = address;
+    this.fonenumber = fonenumber;
+    this.birthday = birthday;
+    this.month = month;
+    this.year = year;
+    this.addInfo = addInfo;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getFonenumber() {
+    return fonenumber;
+  }
+
+  public String getBirthday() {
+    return birthday;
+  }
+
+  public String getMonth() {
+    return month;
+  }
+
+  public String getYear() {
+    return year;
+  }
+
+  public String getAddInfo() {
+    return addInfo;
+  }
+}
